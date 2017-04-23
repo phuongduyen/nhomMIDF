a4e6cd324d68d7565e052371c2f1f09220c1850f
DzungBui <dungbt0108@gmail.com>
2017-04-23 21:37:52 +0700

    Add files via upload

--- EnglishFun_functions.js
+++ EnglishFun_functions.js
@@ -16,14 +16,50 @@ function render(){
 	document.getElementById("h-typeOfQuestions").innerHTML = typeOfQuestions[count];
 	if(typeOfQuestions[count] == "Chọn từ miêu tả hình ảnh"){
 		document.getElementById("h-questions").innerHTML = " ";
+        document.getElementById("anh").style.display = "inline-block";
 		document.getElementById("anh").src = questions[count];
 		document.getElementById("anh").width = "200";
 		document.getElementById("anh").height = "200";
 
-		} else {
-		document.getElementById("h-questions").innerHTML = questions[count];
-	}
+    } else if(typeOfQuestions[count] == "Điền từ tiếng anh phù hợp với nghĩa cho trước"){
+        document.getElementById("h-questions").innerHTML = questions[count];
+    	document.getElementById("blank").style.display = "inline-block";
+        document.getElementById("submit").style.display = "inline-block";
+        document.getElementById("a").style.display = "none";
+    	document.getElementById("b").style.display = "none";
+    	document.getElementById("c").style.display = "none";
+    	document.getElementById("d").style.display = "none";
+        document.getElementById("0").innerHTML = " ";
+        document.getElementById("1").innerHTML = " ";
+        document.getElementById("2").innerHTML = " ";
+        document.getElementById("3").innerHTML = " ";
+        
+        document.getElementById("anh").style.display = "none";
 
+
+        document.getElementById("cauthu").innerHTML = count + 1 + "/" + questions.length;
+
+        return true;
+
+        
+    }else {
+		document.getElementById("h-questions").innerHTML = questions[count];
+        document.getElementById("anh").style.display = "none";
+
+	} 
+    document.getElementById("blank").style.display = "none";
+    document.getElementById("submit").style.display = "none";
+    
+    document.getElementById("a").style.display = "inline-block";
+    document.getElementById("b").style.display = "inline-block";
+	document.getElementById("c").style.display = "inline-block";
+    document.getElementById("d").style.display = "inline-block";
+    
+//    document.getElementById("0").style.display = "inline-block";
+//    document.getElementById("1").style.display = "inline-block";
+//    document.getElementById("2").style.display = "inline-block";
+//    document.getElementById("3").style.display = "inline-block";
+    
 	document.getElementById("a").disabled = false;
 	document.getElementById("b").disabled = false;
 	document.getElementById("c").disabled = false;
@@ -38,6 +74,8 @@ function render(){
 	document.getElementById("b").value = choices[count][1];
 	document.getElementById("c").value = choices[count][2];
 	document.getElementById("d").value = choices[count][3];
+    
+console.log("fb");
 	document.getElementById("cauthu").innerHTML = count + 1 + "/" + questions.length;
 }
 
@@ -51,7 +89,7 @@ function submitAnswer(){
 		} else {
 			useranswers[i].disabled = true;
 		}
-	} 
+	}
 		//kiem tra ket qua cua cau tr loi
 		if(c == answers[count]){
 			console.log("helpp");
@@ -70,59 +108,58 @@ function submitAnswer(){
 		}
 }
 
-//=====================================================================================
+function render2(){
+	if(count == (questions.length -1) ){
+		document.getElementById("end").innerHTML = "Bạn đã hoàn thành khóa học";
+		return true;
+	}
 
+	count++;
+	document.getElementById("demo").innerHTML = " ";
+
+	document.getElementById("h-typeOfQuestions").innerHTML = typeOfQuestions[count];
+	document.getElementById("h-questions").innerHTML = questions[count];
+	document.getElementById("cauthu").innerHTML = count + 1 + "/" + questions.length;
+	// render();
+}
+
+function submitAnswer2(){
+	var c;
+	useranswers = document.getElementById("blank");
+	// console.log("tsughi");
+		//kiem tra ket qua cua cau tr loi
+		c = useranswers.value;
+		//console.log(c);
+		if(c == answers[count]){
+			//console.log("helpp");
+			document.getElementById("demo").innerHTML = "Bạn trả lời đúng rồi!";
+			document.getElementById("demo").style.color = "green";
+			document.getElementById("demo").style.fontSize= "xx-large";
+
+			//console.log(diem);
+			diem = diem + 1;
+			document.getElementById("diem").innerHTML = diem;
+		} 
+		else{
+			document.getElementById("demo").innerHTML = "Bạn trả lời sai rồi! <br> Đáp án đúng là: " + answers[count];
+			document.getElementById("demo").style.color = "red";
+			document.getElementById("demo").style.fontSize = 'xx-large';
+		}
+}
+
+
+
+////bo tu vung
 var count1 =0;
-var btv = 0;
-
 function render1(){
-	if(count1 == (voca[btv].length)){
-		document.getElementById("end").innerHTML = "Chúc mừng bạn đã học xong bộ từ vựng này";
+	if(count1 == (voca.length)){
+		document.getElementById("end").innerHTML = "Bạn đã học xong bộ từ vựng";
 		document.getElementById("end").style.color = "green";
 		return true;
 	}
-	document.getElementById("voca").innerHTML = voca[btv][count1];
-	document.getElementById("image").src = image[btv][count1];
-	document.getElementById("pronounce").innerHTML = pronounce[btv][count1];
-
-	document.getElementById("example0").innerHTML = example[btv][count1][0];
-	document.getElementById("trans0").innerHTML = trans[btv][count1][0];
-	document.getElementById("example1").innerHTML = example[btv][count1][1];
-	document.getElementById("trans1").innerHTML = trans[btv][count1][1];
-	document.getElementById("example2").innerHTML = example[btv][count1][2];
-	document.getElementById("trans2").innerHTML = trans[btv][count1][2];
-
+	document.getElementById("image").src = image[count1];
+	document.getElementById("voca").innerHTML = voca[count1];
+	document.getElementById("pronounce").innerHTML = pronounce[count1];
+	document.getElementById("example").innerHTML = example[count1];
 	count1++;
-}
-
-function btv0(){
-	document.getElementById("end").innerHTML = "";
-	document.getElementById("end").style.color = "";
-	count1 = 0;
-	btv = 0;
-	render1();
-}
-
-function btv1(){
-	document.getElementById("end").innerHTML = "";
-	document.getElementById("end").style.color = "";
-	count1 = 0;
-	btv = 1;
-	render1();
-}
-
-function btv2(){
-	document.getElementById("end").innerHTML = "";
-	document.getElementById("end").style.color = "";
-	count1 = 0;
-	btv = 2;
-	render1();
-}
-
-function btv3(){
-	document.getElementById("end").innerHTML = "";
-	document.getElementById("end").style.color = "";
-	count1 = 0;
-	btv = 3;
-	render1();
 }
